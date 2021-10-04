0d33525379aa60bbd31ac33654f3de108308ad0b
viniosilva <oliveira.vinicius.92@gmail.com>
2021-10-04 18:28:44 -0300

    Nestjs Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './application/health/health.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [ConfigModule.forRoot({ envFilePath: '.config.env' }), HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
